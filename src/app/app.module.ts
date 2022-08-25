a67987d4b9da046ff3921955f5cc497c9c952ac8
Luis Leal <luis-angel.lealhernandez.external@atos.net>
2022-08-25 11:31:08 -0500

    Basic project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { TableComponent } from './components/table/table.component';
+import { ControlsComponent } from './components/controls/controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TableComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule
